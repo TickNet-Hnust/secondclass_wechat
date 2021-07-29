b458f7761f43baed3820a841d965208b63bab817
Peter roe <peterroe@163.com>
2021-07-29 19:14:51 +0800

    'feat:activityCustom'

--- pages/activity-custom/activity-custom.js
+++ pages/activity-custom/activity-custom.js
@@ -1,19 +1,13 @@
-// pages/activity-release/activity-release.js
+// pages/activity-custom/activity-custom.js
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-		active: 'a'
+
 	},
-	switchTab(e) {
-		console.log(e.detail.title)
-		if(e.detail.title == '自定义')
-		wx.navigateTo({
-		  url: '../activity-custom/activity-custom',
-		})
-	},
+
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
@@ -25,26 +19,20 @@ Page({
 	 * 生命周期函数--监听页面初次渲染完成
 	 */
 	onReady: function () {
-		this.selectComponent('#tabs').resize();
+
 	},
 
 	/**
 	 * 生命周期函数--监听页面显示
 	 */
 	onShow: function () {
-		this.setData({     //解决bug做出的奇怪行为1
-			active : 'a'     
-		})
-	},	
+
+	},
 
 	/**
 	 * 生命周期函数--监听页面隐藏
 	 */
 	onHide: function () {
-		this.setData({     //解决bug做出的奇怪行为2
-			active : 'c'
-		})
-		this.selectComponent('#tabs').resize();
 
 	},
 

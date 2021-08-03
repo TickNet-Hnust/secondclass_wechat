6071f52532ffc9cc77c9e016767eab9958b59d22
Peter roe <peterroe@163.com>
2021-08-03 22:41:02 +0800

    update:pages

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ App({
     wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    wx.qy.login({
+			success: function(res) {
+				console.log(res)
+				if (res.code) {
+				  //发起网络请求
+				  wx.request({
+					url: 'https://loxg.top/loginByCode',
+					data: {
+					  code: res.code
+					}
+				  })
+				} else {
+				  console.log('登录失败！' + res.errMsg)
+				}
+				
+			}
+		})
   },
   globalData: {
     userInfo: null
